8633cb66f2935e590ee776ba70d66e1589689459
DESKTOP-1L8RPLH\User <sadzid.maric@edu.fit.ba>
2025-11-08 18:45:48 +0100

    SelmaPrviPush

--- Market.Backend/Market.Application/Modules/Catalog/ProductCategories/Commands/Create/CreateProductCategoryCommandValidator.cs
+++ Market.Backend/Market.Application/Modules/Catalog/ProductCategories/Commands/Create/CreateProductCategoryCommandValidator.cs
@@ -3,10 +3,10 @@
 public sealed class CreateProductCategoryCommandValidator
     : AbstractValidator<CreateProductCategoryCommand>
 {
-    public CreateProductCategoryCommandValidator()
-    {
-        RuleFor(x => x.Name)
-            .NotEmpty().WithMessage("Name is required.")
-            .MaximumLength(ProductCategoryEntity.Constraints.NameMaxLength).WithMessage($"Name can be at most {ProductCategoryEntity.Constraints.NameMaxLength} characters long.");
-    }
+    //public CreateProductCategoryCommandValidator()
+    //{
+    //    RuleFor(x => x.Name)
+    //        .NotEmpty().WithMessage("Name is required.")
+    //        .MaximumLength(ProductCategoryEntity.Constraints.NameMaxLength).WithMessage($"Name can be at most {ProductCategoryEntity.Constraints.NameMaxLength} characters long.");
+    //}
 }
--- Market.Backend/Market.Infrastructure/Database/DatabaseContext.cs
+++ Market.Backend/Market.Infrastructure/Database/DatabaseContext.cs
@@ -4,8 +4,7 @@ namespace Market.Infrastructure.Database;
 
 public partial class DatabaseContext : DbContext, IAppDbContext
 {
-    public DbSet<ProductCategoryEntity> ProductCategories => Set<ProductCategoryEntity>();
-    public DbSet<ProductEntity> Products => Set<ProductEntity>();
+    
     public DbSet<MarketUserEntity> Users => Set<MarketUserEntity>();
     public DbSet<RefreshTokenEntity> RefreshTokens => Set<RefreshTokenEntity>();
 
