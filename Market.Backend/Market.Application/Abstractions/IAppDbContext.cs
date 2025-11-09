56679dc46889fbd55570ce5ee2bcb652ed2ce7ae
Selma Baralija <selmabaralija@gmail.com>
2025-11-09 23:55:28 +0100

    Commite iapp

--- Market.Backend/Market.Application/Abstractions/IAppDbContext.cs
+++ Market.Backend/Market.Application/Abstractions/IAppDbContext.cs
@@ -1,11 +1,56 @@
-﻿namespace Market.Application.Abstractions;
+﻿using Market.Domain.Entities.Civic;
+using Market.Domain.Entities.Events;
+using Market.Domain.Entities.Media;
+using Market.Domain.Entities.Reports;
+using Market.Domain.Entities.Rewards;
+using Market.Domain.Entities.Surveys;
+using Market.Domain.Entities.Volunteering;
+
+namespace Market.Application.Abstractions;
 
 // Application layer
 public interface IAppDbContext
 {
-    
+    // ===== Identity =====
     DbSet<MarketUserEntity> Users { get; }
     DbSet<RefreshTokenEntity> RefreshTokens { get; }
+    DbSet<ProfileEntity> Profiles { get; }
+    DbSet<RoleEntity> Roles { get; }
+    DbSet<UserRoleEntity> UserRoles { get; }
+
+
+    // ===== Reports =====
+    DbSet<ProblemReportEntity> ProblemReports { get; }
+    DbSet<ProblemCategoryEntity> ProblemCategories { get; }
+    DbSet<ProblemStatusEntity> ProblemStatuses { get; }
+    DbSet<CommentEntity> Comments { get; }
+    DbSet<TaskEntity> Tasks { get; }
+    DbSet<ProofOfResolutionEntity> ProofsOfResolution { get; }
+    DbSet<RatingEntity> Ratings { get; }
+
+    // ===== Media =====
+    DbSet<MediaAttachmentEntity> MediaAttachments { get; }
+    DbSet<MediaLinkEntity> MediaLinks { get; }
+
+    // ===== Surveys =====
+    DbSet<SurveyEntity> Surveys { get; }
+    DbSet<SurveyResponseEntity> SurveyResponses { get; }
+
+    // ===== Rewards =====
+    DbSet<RewardEntity> Rewards { get; }
+    DbSet<AssignedRewardEntity> AssignedRewards { get; }
+
+    // ===== Civic =====
+    DbSet<CitizenProposalEntity> CitizenProposals { get; }
+    DbSet<WatchListEntity> WatchLists { get; }
+
+    // ===== Events =====
+    DbSet<EventCalendarEntity> EventsCalendar { get; }
+
+    // ===== Volunteering =====
+    DbSet<VolunteerActionEntity> VolunteerActions { get; }
+    DbSet<ActionParticipantEntity> ActionParticipants { get; }
+
 
     Task<int> SaveChangesAsync(CancellationToken ct);
 }
