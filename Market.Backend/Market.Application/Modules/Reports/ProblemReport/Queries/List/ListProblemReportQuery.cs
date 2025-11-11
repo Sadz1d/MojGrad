33b270b4cf450ac1b4f87751f735ac8d02de33e7
Lamka <lamijamerzic1234@gmail.com>
2025-11-11 14:52:28 +0100

    Commit ListProblemReport

--- Market.Backend/Market.Application/Modules/Reports/ProblemReport/Queries/List/ListProblemReportQuery.cs
+++ Market.Backend/Market.Application/Modules/Reports/ProblemReport/Queries/List/ListProblemReportQuery.cs
@@ -4,9 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Market.Application.Modules.Reports.ProblemReport.Queries.List
+using Market.Application.Modules.Reports.ProblemReport.Queries.List;
+
+namespace Market.Application.Modules.Reports.ProblemReport.Queries.List;
+
+public sealed class ListProblemReportQuery : BasePagedQuery<ListProblemReportQueryDto>
 {
-    internal class ListProblemReportQuery
-    {
-    }
+    public string? Search { get; init; }     // po Title/Description
+    public int? UserId { get; init; }        // autor
+    public int? CategoryId { get; init; }    // kategorija
+    public int? StatusId { get; init; }      // status
 }
+
