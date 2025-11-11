3c65d1d4e3694e30d57cdf433ff51e88cbcc542d
Lamka <lamijamerzic1234@gmail.com>
2025-11-11 15:00:13 +0100

    Commit CretaeProblemReportFix

--- Market.Backend/Market.Application/Modules/Reports/ProblemReport/Queries/List/ListProblemReportQuery.cs
+++ Market.Backend/Market.Application/Modules/Reports/ProblemReport/Queries/List/ListProblemReportQuery.cs
@@ -4,15 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-using Market.Application.Modules.Reports.ProblemReport.Queries.List;
-
-namespace Market.Application.Modules.Reports.ProblemReport.Queries.List;
-
-public sealed class ListProblemReportQuery : BasePagedQuery<ListProblemReportQueryDto>
+namespace Market.Application.Modules.Reports.ProblemReport.Queries.List
 {
-    public string? Search { get; init; }     // po Title/Description
-    public int? UserId { get; init; }        // autor
-    public int? CategoryId { get; init; }    // kategorija
-    public int? StatusId { get; init; }      // status
+    internal class ListProblemReportQuery
+    {
+    }
 }
-
--- Market.Backend/Market.Application/Modules/Reports/ProblemReport/Queries/List/ListProblemReportQueryDto.cs
+++ Market.Backend/Market.Application/Modules/Reports/ProblemReport/Queries/List/ListProblemReportQueryDto.cs
@@ -4,19 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Market.Application.Modules.Reports.ProblemReport.Queries.List;
-
-public sealed class ListProblemReportQueryDto
+namespace Market.Application.Modules.Reports.ProblemReport.Queries.List
 {
-    public required int Id { get; init; }
-    public required string Title { get; init; }
-    public required string AuthorName { get; init; }
-    public required DateTime CreationDate { get; init; }
-    public string? Location { get; init; }
-    public required string CategoryName { get; init; }
-    public required string StatusName { get; init; }
-    public required int CommentsCount { get; init; }
-    public required int TasksCount { get; init; }
-    public required int RatingsCount { get; init; }
-    public string? ShortDescription { get; init; }
+    internal class ListProblemReportQueryDto
+    {
+    }
 }
