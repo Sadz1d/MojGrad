bb5d60757db387f0c947934477004350ad875298
DESKTOP-9K327VT\sad <maricsadzid@gmail.com>
2025-12-29 19:25:07 +0100

    Commit-ProfileEntity-Create-Update-validators

---
+++ Market.Backend/Market.Application/Modules/Identity/Profiles/Commands/Create/CreateProfileCommandValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using Market.Application.Modules.Identity.Profiles.Commands.Create;
+
+namespace Market.Application.Modules.Identity.Profile.Commands.Create;
+
+public sealed class CreateProfileCommandValidator
+    : AbstractValidator<CreateProfileCommand>
+{
+    public CreateProfileCommandValidator()
+    {
+        RuleFor(x => x.UserId)
+            .GreaterThan(0);
+
+        RuleFor(x => x.Phone)
+            .MaximumLength(30);
+
+        RuleFor(x => x.Address)
+            .MaximumLength(250);
+
+        RuleFor(x => x.BiographyText)
+            .MaximumLength(1000);
+    }
+}
---
+++ Market.Backend/Market.Application/Modules/Identity/Profiles/Commands/Update/UpdateProfileCommandValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using Market.Application.Modules.Identity.Profiles.Commands.Update;
+
+namespace Market.Application.Modules.Identity.Profile.Commands.Update;
+
+public sealed class UpdateProfileCommandValidator
+    : AbstractValidator<UpdateProfileCommand>
+{
+    public UpdateProfileCommandValidator()
+    {
+        RuleFor(x => x.Id)
+            .GreaterThan(0);
+
+        RuleFor(x => x.Phone)
+            .MaximumLength(30);
+
+        RuleFor(x => x.Address)
+            .MaximumLength(250);
+
+        RuleFor(x => x.BiographyText)
+            .MaximumLength(1000);
+    }
+}
