80636051013e7d34d9898b762b24c553fb340846
DESKTOP-9K327VT\sad <maricsadzid@gmail.com>
2025-12-29 15:44:16 +0100

    Commit-comment-update-create-validators

---
+++ Market.Backend/Market.Application/Modules/Reports/Comment/Command/Create/CreateCommentCommandValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using Market.Domain.Entities.Reports;
+
+namespace Market.Application.Modules.Reports.Comments.Commands.Create;
+
+public sealed class CreateCommentCommandValidator
+    : AbstractValidator<CreateCommentCommand>
+{
+    public CreateCommentCommandValidator()
+    {
+        RuleFor(x => x.ReportId)
+            .GreaterThan(0);
+
+        RuleFor(x => x.Text)
+            .NotEmpty().WithMessage("Comment text is required.")
+            .MaximumLength(CommentEntity.Constraints.TextMaxLength)
+            .WithMessage($"Comment can be at most {CommentEntity.Constraints.TextMaxLength} characters long.");
+    }
+}
