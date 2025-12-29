2f5a867a0ef30c1a4553edb8289513e2ad177fc2
Selma Baralija <selmabaralija@gmail.com>
2025-12-29 20:09:26 +0100

    Seeder_and_GlobalUsing

--- Market.Backend/Market.Infrastructure/Database/Seeders/StaticDataSeeder.cs
+++ Market.Backend/Market.Infrastructure/Database/Seeders/StaticDataSeeder.cs
@@ -1,35 +1,107 @@
-﻿namespace Market.Infrastructure.Database.Seeders;
+﻿//namespace Market.Infrastructure.Database.Seeders;
 
-public partial class StaticDataSeeder
+//public partial class StaticDataSeeder
+//{
+//    private static DateTime DateTime { get; set; } = new DateTime(2022, 4, 13, 1, 22, 18, 866, DateTimeKind.Local);
+
+//    public static void Seed(ModelBuilder modelBuilder)
+//    {
+//        // Static data is added in the migration
+//        // if it does not exist in the DB at the time of creating the migration
+//        // example of static data: roles
+//        SeedProductCategories(modelBuilder);
+//    }
+
+//    private static void SeedProductCategories(ModelBuilder modelBuilder)
+//    {
+//        // todo: user roles
+
+//        //modelBuilder.Entity<UserRoles>().HasData(new List<UserRoleEntity>
+//        //{
+//        //    new UserRoleEntity{
+//        //        Id = 1,
+//        //        Name = "Admin",
+//        //        CreatedAt = dateTime,
+//        //        ModifiedAt = null,
+//        //    },
+//        //    new UserRoleEntity{
+//        //        Id = 2,
+//        //        Name = "Employee",
+//        //        CreatedAt = dateTime,
+//        //        ModifiedAt = null,
+//        //    },
+//        //});
+//    }
+//}
+using Market.Domain.Entities.Reports;
+
+namespace Market.Infrastructure.Database.Seeders;
+
+public static class StaticDataSeeder
 {
-    private static DateTime DateTime { get; set; } = new DateTime(2022, 4, 13, 1, 22, 18, 866, DateTimeKind.Local);
+    private static readonly DateTime SeedDate =
+        new DateTime(2024, 1, 1, 0, 0, 0, DateTimeKind.Utc);
 
     public static void Seed(ModelBuilder modelBuilder)
     {
-        // Static data is added in the migration
-        // if it does not exist in the DB at the time of creating the migration
-        // example of static data: roles
-        SeedProductCategories(modelBuilder);
+        SeedProblemStatuses(modelBuilder);
+        SeedProblemCategories(modelBuilder);
     }
 
-    private static void SeedProductCategories(ModelBuilder modelBuilder)
+    private static void SeedProblemStatuses(ModelBuilder modelBuilder)
     {
-        // todo: user roles
+        modelBuilder.Entity<ProblemStatusEntity>().HasData(
+            new ProblemStatusEntity
+            {
+                Id = 1,
+                Name = "Prijavljeno",
+                CreatedAtUtc = SeedDate,
+                IsDeleted = false
+            },
+            new ProblemStatusEntity
+            {
+                Id = 2,
+                Name = "U toku",
+                CreatedAtUtc = SeedDate,
+                IsDeleted = false
+            },
+            new ProblemStatusEntity
+            {
+                Id = 3,
+                Name = "Riješeno",
+                CreatedAtUtc = SeedDate,
+                IsDeleted = false
+            }
+        );
+    }
 
-        //modelBuilder.Entity<UserRoles>().HasData(new List<UserRoleEntity>
-        //{
-        //    new UserRoleEntity{
-        //        Id = 1,
-        //        Name = "Admin",
-        //        CreatedAt = dateTime,
-        //        ModifiedAt = null,
-        //    },
-        //    new UserRoleEntity{
-        //        Id = 2,
-        //        Name = "Employee",
-        //        CreatedAt = dateTime,
-        //        ModifiedAt = null,
-        //    },
-        //});
+    private static void SeedProblemCategories(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<ProblemCategoryEntity>().HasData(
+            new ProblemCategoryEntity
+            {
+                Id = 1,
+                Name = "Saobraćaj",
+                Description = "Problemi na cestama",
+                CreatedAtUtc = SeedDate,
+                IsDeleted = false
+            },
+            new ProblemCategoryEntity
+            {
+                Id = 2,
+                Name = "Otpad",
+                Description = "Smeće i kontejneri",
+                CreatedAtUtc = SeedDate,
+                IsDeleted = false
+            },
+            new ProblemCategoryEntity
+            {
+                Id = 3,
+                Name = "Rasvjeta",
+                Description = "Ulična rasvjeta",
+                CreatedAtUtc = SeedDate,
+                IsDeleted = false
+            }
+        );
     }
 }
--- Market.Backend/Market.Infrastructure/GlobalUsings.cs
+++ Market.Backend/Market.Infrastructure/GlobalUsings.cs
@@ -1,5 +1,30 @@
-﻿global using Microsoft.EntityFrameworkCore;
+﻿// EF Core
+global using Microsoft.EntityFrameworkCore;
 global using Microsoft.EntityFrameworkCore.Metadata.Builders;
-//global using Market.Domain.Entities.Catalog;
-global using Market.Domain.Entities.Identity;
+
+// Identity & Security
 global using Microsoft.AspNetCore.Identity;
+
+// Domain – common
+global using Market.Domain.Common;
+
+// Domain – Identity
+global using Market.Domain.Entities.Identity;
+
+// Domain – Civic
+global using Market.Domain.Entities.Civic;
+
+// Domain – Reports
+global using Market.Domain.Entities.Reports;
+
+// Domain – Surveys
+global using Market.Domain.Entities.Surveys;
+
+// Domain – Rewards
+global using Market.Domain.Entities.Rewards;
+
+// Domain – Volunteering
+global using Market.Domain.Entities.Volunteering;
+
+// Domain – Events
+global using Market.Domain.Entities.Events;
