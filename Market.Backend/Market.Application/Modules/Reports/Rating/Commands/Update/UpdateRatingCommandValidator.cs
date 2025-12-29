fede24d96eb1970bd06c7cca716b66331b87e1e5
DESKTOP-9K327VT\sad <maricsadzid@gmail.com>
2025-12-29 15:32:13 +0100

    Commit-rating-update-create-validators

---
+++ Market.Backend/Market.Application/Modules/Reports/Rating/Commands/Update/UpdateRatingCommandValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using Market.Application.Modules.Report.Ratings.Commands.Update;
+using Market.Domain.Entities.Reports;
+
+namespace Market.Application.Modules.Reports.Ratings.Commands.Update;
+
+public sealed class UpdateRatingCommandValidator
+    : AbstractValidator<UpdateRatingCommand>
+{
+    public UpdateRatingCommandValidator()
+    {
+        RuleFor(x => x.Id)
+            .GreaterThan(0);
+
+        RuleFor(x => x.Rating)
+            .InclusiveBetween(1, 5)
+            .WithMessage("Rating must be between 1 and 5.");
+
+        RuleFor(x => x.RatingComment)
+            .MaximumLength(RatingEntity.Constraints.CommentMaxLength)
+            .WithMessage($"Comment can be at most {RatingEntity.Constraints.CommentMaxLength} characters long.");
+    }
+}
